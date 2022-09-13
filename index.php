4cba150e10910ec34c00f4e29b49ac13e8c31c14
Pluk Zwaal <Plukky.zwaal@gmail.com>
2022-09-13 11:54:26 +0200

    Basic start of the website

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Pra1</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<header>
+
+	</header>
+	<body>
+		<div class="sectionBlocks">
+			<div class="wrapper">
+				<h2>Hoe werken de blokken?</h2>
+				<div class="sectionBlocksFlex">
+					<img src="img/blocks.png">
+					<p>Lorem ipsum, dolor sit amet,con sectetur adipisicing elit. Laborum culpa, eligendi ea quam impedit exercitationem expedita voluptates soluta beatae doloremque nihil at, atque nostrum ullam tempore! Dolorem voluptate ipsam reprehenderit. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit quo debitis explicabo nemo incidunt animi expedita quaerat id excepturi neque? Deserunt ipsam temporibus porro, quo consequuntur explicabo quae ex dolorum.</p>
+				</div>
+			</div>
+		</div>
+		
+	</body>
+	<footer>
+
+	</footer>
+</body>
+</html>
