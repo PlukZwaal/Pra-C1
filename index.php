d66916a93a3b5e523586d2110c13ae6df9d6bf23
Pluk Zwaal <Plukky.zwaal@gmail.com>
2022-09-14 10:24:29 +0200

    added versnellen section

--- index.php
+++ index.php
@@ -8,6 +8,10 @@
 </head>
 <body>
 	<header>
+		<div class="sectionHeader">
+			<a href="">Blokken</a>
+			<a href="">Versnellen</a>
+		</div>
 
 	</header>
 	<body>
@@ -16,11 +20,31 @@
 				<h2>Hoe werken de blokken?</h2>
 				<div class="sectionBlocksFlex">
 					<img src="img/blocks.png">
-					<p>Lorem ipsum, dolor sit amet,con sectetur adipisicing elit. Laborum culpa, eligendi ea quam impedit exercitationem expedita voluptates soluta beatae doloremque nihil at, atque nostrum ullam tempore! Dolorem voluptate ipsam reprehenderit. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit quo debitis explicabo nemo incidunt animi expedita quaerat id excepturi neque? Deserunt ipsam temporibus porro, quo consequuntur explicabo quae ex dolorum.</p>
+					<div class="sectionBlocksFlex2">
+						<p>Lorem ipsum, dolor sit amet,con sectetur adipisicing elit. Laborum culpa, eligendi ea quam impedit exercitationem expedita voluptates soluta beatae doloremque nihil at, atque nostrum ullam tempore! Dolorem voluptate ipsam reprehenderit. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit quo debitis explicabo nemo incidunt animi expedita quaerat id excepturi neque? Deserunt ipsam temporibus porro, quo consequuntur explicabo quae ex dolorum.</p>
+						<p>Lorem ipsum, dolor sit amet,con sectetur adipisicing elit. Laborum culpa, eligendi ea quam impedit exercitationem expedita voluptates soluta beatae doloremque nihil at, atque nostrum ullam tempore! Dolorem voluptate ipsam reprehenderit. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit quo debitis explicabo nemo incidunt animi expedita quaerat id excepturi neque? Deserunt ipsam temporibus porro, quo consequuntur explicabo quae ex dolorum.</p>
+					</div>
 				</div>
 			</div>
 		</div>
-		
+		<div class="sectionAccelerated">
+			<div class="wrapper">
+				<h2>Hoe kan je versnellen?</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</div>
+		</div>
+		<div class="sectionSubjects">
+			<div class="wrapper">
+				<ul>
+					<li>eee</li>
+				</ul>
+			</div>
+		</div>
 	</body>
 	<footer>
 
