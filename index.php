e98cdcd1371d788ceee2876ee57960cf528720e2
Pluk Zwaal <Plukky.zwaal@gmail.com>
2022-09-14 15:59:17 +0200

    added Header, Homework, Schedule, Teachers

--- index.php
+++ index.php
@@ -9,13 +9,19 @@
 <body>
 	<header>
 		<div class="sectionHeader">
-			<a href="">Blokken</a>
-			<a href="">Versnellen</a>
+			<img src="img/curio-02-wit-logo-rgb.png">
+			<div class="sectionHeader2">
+				<a href="#blocks">Blokken</a>
+				<a href="#accelerated">Versnellen</a>
+				<a href="#subjects">Vakken</a>
+				<a href="#homework">Huiswerk</a>
+				<a href="#schedule">Rooster</a>
+				<a href="#teachers">Docenten</a>
+			</div>
 		</div>
-
 	</header>
 	<body>
-		<div class="sectionBlocks">
+		<div id="blocks" class="sectionBlocks">
 			<div class="wrapper">
 				<div class="sectionBlocksFlex">
 					<div class="sectionBlocksFlex2">
@@ -26,8 +32,8 @@
 					<img src="img/blocks.png">
 				</div>
 			</div>
-		</div>
-		<div class="sectionAccelerated">
+		</div> 
+		<div id="accelerated" class="sectionAccelerated">
 			<div class="wrapper">
 				<h2>Hoe kan je versnellen?</h2>
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
@@ -38,16 +44,115 @@
 				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 			</div>
 		</div>
-		<div class="sectionSubjects">
+		<div id="subjects" class="sectionSubjects">
 			<div class="wrapper">
-				<ul>
-					<li>eeeee</li>
-				</ul>
+				<h2>Vakken</h2>
+				<div class="subject">
+					<b>Native</b>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque ipsum ducimus asperiores excepturi quia at temporibus perferendis illum inventore saepe tempore exercitationem nobis dolores dolorem, dolor iusto, reiciendis iste, sapiente.
+				</div>
+				<div class="subject">
+					<b>Web-Development</b>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque ipsum ducimus asperiores excepturi quia at temporibus perferendis illum inventore saepe tempore exercitationem nobis dolores dolorem, dolor iusto, reiciendis iste, sapiente.
+				</div>
+				<div class="subject">
+					<b>PraktijkWerk</b>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque ipsum ducimus asperiores excepturi quia at temporibus perferendis illum inventore saepe tempore exercitationem nobis dolores dolorem, dolor iusto, reiciendis iste, sapiente.
+				</div>
+				<div class="subject">
+					<b>Praktijkondersteuning</b>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque ipsum ducimus asperiores excepturi quia at temporibus perferendis illum inventore saepe tempore exercitationem nobis dolores dolorem, dolor iusto, reiciendis iste, sapiente.
+				</div>
+				<div class="subject">
+					<b>Portfolio</b>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque ipsum ducimus asperiores excepturi quia at temporibus perferendis illum inventore saepe tempore exercitationem nobis dolores dolorem, dolor iusto, reiciendis iste, sapiente.
+				</div>
+				<div class="subject">
+					<b>Studieloopbaanbegeiding</b>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque ipsum ducimus asperiores excepturi quia at temporibus perferendis illum inventore saepe tempore exercitationem nobis dolores dolorem, dolor iusto, reiciendis iste, sapiente.
+				</div>
+				<div class="subject">
+					<b>Burgerschap</b>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque ipsum ducimus asperiores excepturi quia at temporibus perferendis illum inventore saepe tempore exercitationem nobis dolores dolorem, dolor iusto, reiciendis iste, sapiente.
+				</div>
+			</div>
+		</div>
+		<div id="homework" class="sectionHomework">
+			<div class="wrapper">
+				<h2>Hoe is het huiswerk?</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</div>
+		</div>
+		<div id="schedule" class="sectionSchedule">
+			<div class="wrapper">
+				<h2>Hoe ziet ons rooster eruit</h2>
+				<div class="sectionScheduleFlex">
+					<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor molestias in adipisci assumenda suscipit, nihil voluptatem asperiores quos placeat, accusantium. Suscipit accusantium quas, maiores minus, eius placeat autem nesciunt omnis?</p>
+					<img src="img/rooster.png">
+				</div>
+			</div>
+		</div>
+		<div id="teachers" class="sectionTeachers">
+			<div class="wrapper">
+				<h2>Docenten</h2>
+				<div class="sectionTeachers2">
+					<div class="teacher">
+						<img src="img/curio-15-licht-groen-logo-rgb.png">
+						<div class="teacherFlex">
+							<b>Lorem</b>
+							<p>Lorem, ipsum, dolor sit amet consectetur adipisicing elit. Doloribus, voluptatibus quo facilis unde temporibus.</p>
+						</div>
+					</div>
+					<div class="teacher">
+						<img src="img/curio-15-licht-groen-logo-rgb.png">
+						<div class="teacherFlex">
+							<b>Lorem</b>
+							<p>Lorem, ipsum, dolor sit amet consectetur adipisicing elit. Doloribus, voluptatibus quo facilis unde temporibus.</p>
+						</div>
+					</div>
+					<div class="teacher">
+						<img src="img/curio-15-licht-groen-logo-rgb.png">
+						<div class="teacherFlex">
+							<b>Lorem</b>
+							<p>Lorem, ipsum, dolor sit amet consectetur adipisicing elit. Doloribus, voluptatibus quo facilis unde temporibus.</p>
+						</div>
+					</div>
+					<div class="teacher">
+						<img src="img/curio-15-licht-groen-logo-rgb.png">
+						<div class="teacherFlex">
+							<b>Lorem</b>
+							<p>Lorem, ipsum, dolor sit amet consectetur adipisicing elit. Doloribus, voluptatibus quo facilis unde temporibus.</p>
+						</div>
+					</div>
+					<div class="teacher">
+						<img src="img/curio-15-licht-groen-logo-rgb.png">
+						<div class="teacherFlex">
+							<b>Lorem</b>
+							<p>Lorem, ipsum, dolor sit amet consectetur adipisicing elit. Doloribus, voluptatibus quo facilis unde temporibus.</p>
+						</div>
+					</div>
+					<div class="teacher">
+						<img src="img/curio-15-licht-groen-logo-rgb.png">
+						<div class="teacherFlex">
+							<b>Lorem</b>
+							<p>Lorem, ipsum, dolor sit amet consectetur adipisicing elit. Doloribus, voluptatibus quo facilis unde temporibus.</p>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	</body>
 	<footer>
-
+		<div class="sectionFooter">
+			<div class="wrapper">
+				<p>Gemaakt door Pluk Zwaal</p>
+			</div>
+		</div>
 	</footer>
 </body>
 </html>
