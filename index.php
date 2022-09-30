81a05d278d06b7539c0546639ff73e95cd51394d
Pluk Zwaal <Plukky.zwaal@gmail.com>
2022-09-30 11:18:57 +0200

    Removed unnesecery images, Added text and testen the site in a html validatitor

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Pra1</title>
+	<title>Curio</title>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
 	<header>
 		<div class="sectionHeader">
-			<img src="img/curio-16-midden-groen-logo-rgb.png">
+			<img src="img/curio-16-midden-groen-logo-rgb.png" alt="">
 			<div class="sectionHeader2">
 				<a href="#blocks">Blokken</a>
 				<a href="#accelerated">Versnellen</a>
@@ -42,7 +42,7 @@
 						<p>Lorem ipsum, dolor sit amet,con sectetur adipisicing elit. Laborum culpa, eligendi ea quam impedit exercitationem expedita voluptates soluta beatae doloremque nihil at, atque nostrum ullam tempore! Dolorem voluptate ipsam reprehenderit. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit quo debitis explicabo nemo incidunt animi expedita quaerat id excepturi neque? Deserunt ipsam temporibus porro, quo consequuntur explicabo quae ex dolorum.</p>
 						<p>Lorem ipsum, dolor sit amet,con sectetur adipisicing elit. Laborum culpa, eligendi ea quam impedit exercitationem expedita voluptates soluta beatae doloremque nihil at, atque nostrum ullam tempore! Dolorem voluptate ipsam reprehenderit. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit quo debitis explicabo nemo incidunt animi expedita quaerat id excepturi neque? Deserunt ipsam temporibus porro, quo consequuntur explicabo quae ex dolorum.</p>
 					</div>
-					<img src="img/blocks.png">
+					<img src="img/blocks.png" alt="">
 				</div>
 			</div>
 		</div> 
@@ -62,55 +62,42 @@
 				<h2>Vakken</h2>
 				<div class="subject">
 					<b>Native</b>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque ipsum ducimus asperiores excepturi quia at temporibus perferendis illum inventore saepe tempore exercitationem nobis dolores dolorem, dolor iusto, reiciendis iste, sapiente.
+					<p>Native: Native is het vak waarbij  er geprogrammeerd wordt met C#. hier worden applicaties gemaakt bijv: een rekenmachine, Pacman en  Pong.</p>
 				</div>
 				<div class="subject">
 					<b>Web-Development</b>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque ipsum ducimus asperiores excepturi quia at temporibus perferendis illum inventore saepe tempore exercitationem nobis dolores dolorem, dolor iusto, reiciendis iste, sapiente.
+					<p>WEB staat voor Web Development hier wordt  met html, CSS en PHP gewerkt. Met html kun je denken aan de voorkant van een pagina. CSS is de styling van de pagina en PHP is de backend, oftewel de database van de pagina.</p>
 				</div>
 				<div class="subject">
 					<b>PraktijkWerk</b>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque ipsum ducimus asperiores excepturi quia at temporibus perferendis illum inventore saepe tempore exercitationem nobis dolores dolorem, dolor iusto, reiciendis iste, sapiente.
+					<p>Is een praktijkles waarbij gewerkt wordt aan een project met je klasgenoten. Bij deze wordt geoefend op zelfstandigheid en teamwerk.</p>
 				</div>
 				<div class="subject">
 					<b>Praktijkondersteuning</b>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque ipsum ducimus asperiores excepturi quia at temporibus perferendis illum inventore saepe tempore exercitationem nobis dolores dolorem, dolor iusto, reiciendis iste, sapiente.
+					<p>Hier wordt het plannen van het project gedaan en methodes meegegeven die helpen bij het vervullen van de opdracht. Ook wel bekend als het saaiste vak op school</p>
 				</div>
 				<div class="subject">
 					<b>Portfolio</b>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque ipsum ducimus asperiores excepturi quia at temporibus perferendis illum inventore saepe tempore exercitationem nobis dolores dolorem, dolor iusto, reiciendis iste, sapiente.
+					<p>Op de middelbare school noemen ze deze les ook wel MentorUur. 1x in de 1/2 maanden moet je hierheen komen om met je docent te praten hoe het met je gaat en hoe je er voorstaat etc.
 				</div>
 				<div class="subject">
-					<b>Studieloopbaanbegeiding</b>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque ipsum ducimus asperiores excepturi quia at temporibus perferendis illum inventore saepe tempore exercitationem nobis dolores dolorem, dolor iusto, reiciendis iste, sapiente.
-				</div>
-				<div class="subject">
-					<b>Burgerschap</b>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque ipsum ducimus asperiores excepturi quia at temporibus perferendis illum inventore saepe tempore exercitationem nobis dolores dolorem, dolor iusto, reiciendis iste, sapiente.
-				</div>
-				<div class="subject">
-					<b>Codelab</b>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque ipsum ducimus asperiores excepturi quia at temporibus perferendis illum inventore saepe tempore exercitationem nobis dolores dolorem, dolor iusto, reiciendis iste, sapiente.
+					<b>Delta</b>
+					<p>Dit is een les, waarbij je extra stof ontvangt over alle vakken. Dit vak wordt gegeven aan je als je een versnelt traject doet van de opleiding.</p>
 				</div>
 			</div>
 		</div>
 		<div id="homework" class="sectionHomework">
 			<div class="wrapper">
 				<h2>Hoe is het huiswerk?</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<p>Het huiswerk op deze opleiding is erg fijn. Bij bijna elk vak hoef je het pas zondag de laatste dag van de week inteleveren waardoor je in het weekend er nog aan kan werken als je het niet af krijgt in de les. Meestal krijg je wel genoeg tijd maar soms is dat niet mogelijk en moet je er thuis aan werken.</p>
 			</div>
 		</div>
 		<div id="schedule" class="sectionSchedule">
 			<div class="wrapper">
 				<h2>Hoe ziet ons rooster eruit?</h2>
 				<div class="sectionScheduleFlex">
-					<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor molestias in adipisci assumenda suscipit, nihil voluptatem asperiores quos placeat, accusantium. Suscipit accusantium quas, maiores minus, eius placeat autem nesciunt omnis?</p>
-					<img src="img/rooster.png">
+					<p>Ons rooster is na mijn mening verschrikkelijk. Je begint elke dag niet eerder dan half 9 en bent vaak laat uit. Er zijn ook veel te veel pauzes op deze opleiding. Elke ochtend 1x 15m, In de middag 1x 30 minuten wat soms 1 uur is omdat anders de lessen niet uit kunnen komen en nog 1x 15m. Ik vind dit zelf niet fijn want ik heb die pauzes net nodig. Vaak sturen docenten je wel eerder naar huis toe als je tot laat moet en dat is wel erg fijn.</p>
+					<img src="img/rooster.png" alt="">
 				</div>
 			</div>
 		</div>
@@ -119,73 +106,59 @@
 				<h2>Docenten</h2>
 				<div class="sectionTeachers2">
 					<div class="teacher">
-						<img src="img/steven.png">
+						<img src="img/steven.png" alt="">
 						<div class="teacherFlex">
 							<b>Steven Van Roosendaal</b>
-							<p>Lorem, ipsum, dolor sit amet consectetur adipisicing elit. Doloribus, voluptatibus quo facilis unde temporibus.</p>
+							<p>Steven is web/pra/pro docent. Steven kan heel goed les kan geven, maar soms is de les iets te veel uitleg en te weinig interactie met de klas waardoor de les een stuk minder leuk wordt. Daardoor wordt er soms minder opgelet tijdens zijn lessen.</p>
 						</div>
 					</div>
 					<div class="teacher">
-						<img src="img/wicher.png">
+						<img src="img/wicher.png" alt="">
 						<div class="teacherFlex">
 							<b>Wicher Hulzebosch </b>
-							<p>Lorem, ipsum, dolor sit amet consectetur adipisicing elit. Doloribus, voluptatibus quo facilis unde temporibus.</p>
+							<p>Wicher is pra/native/burgerschap docent. Wicher is een docent die heel goed weet wat hij moet doen maar soms mist er een kleine voorbereiding voor de les. Want als Wicher de les binnen komt dan moet hij aan ons vragen waar we waren gebleven.</p>
 						</div>
 					</div>
 					<div class="teacher">
-						<img src="img/corne.png">
-						<div class="teacherFlex">
-							<b>Corne Wagtmans</b>
-							<p>Lorem, ipsum, dolor sit amet consectetur adipisicing elit. Doloribus, voluptatibus quo facilis unde temporibus.</p>
-						</div>
-					</div>
-					<div class="teacher">
-						<img src="img/fedje.png">
+						<img src="img/fedje.png" alt="">
 						<div class="teacherFlex">
 							<b>Fedde van Gills</b>
-							<p>Lorem, ipsum, dolor sit amet consectetur adipisicing elit. Doloribus, voluptatibus quo facilis unde temporibus.</p>
+							<p>Fedde is web docent. Fedde zijn les bestaat uit uitleg en het gelijk mee doen en meeschrijven. Voorderest heb ik de docent pas een paar weken dus kan er niet heel veel over vertellen.</p>
 						</div>
 					</div>
 					<div class="teacher">
-						<img src="img/tim.png">
+						<img src="img/tim.png" alt="">
 						<div class="teacherFlex">
 							<b>Tim Lutt</b>
-							<p>Lorem, ipsum, dolor sit amet consectetur adipisicing elit. Doloribus, voluptatibus quo facilis unde temporibus.</p>
+							<p>Tim is Native docent. Tim is een heel gezellige docent waar je altijd wel een grapje mee kan uithalen. Maar op een gegeven moment is het wel klaar en moet je ook serieus te werk gaan. Zijn lessen bestaan uit het lezen van het boekje en dan opdrachten maken.</p>
 						</div>
 					</div>
 					<div class="teacher">
-						<img src="img/ine.png">
+						<img src="img/ine.png" alt="">
 						<div class="teacherFlex">
 							<b>Ine Vermeer</b>
-							<p>Lorem, ipsum, dolor sit amet consectetur adipisicing elit. Doloribus, voluptatibus quo facilis unde temporibus.</p>
+							<p>Ine is een web docent. Zij is erg goed in dit vak en heeft zelfs een eigen blog website.</p>
 						</div>
 					</div>
 					<div class="teacher">
-						<img src="img/wiro.png">
+						<img src="img/wiro.png" alt="">
 						<div class="teacherFlex">
 							<b>Wiro Verdiesen</b>
-							<p>Lorem, ipsum, dolor sit amet consectetur adipisicing elit. Doloribus, voluptatibus quo facilis unde temporibus.</p>
+							<p>Wiro is Reken docent. Wiro zijn les geven bestond eerst uit een deel uitleg en dan zelfstandig werken. Maar nu is de les gewoon zelfstandig werken en als je vragen hebt dan kun je altijd bij hem terecht.</p>
 						</div>
 					</div>
 					<div class="teacher">
-						<img src="img/mustafa.png">
+						<img src="img/mustafa.png" alt="">
 						<div class="teacherFlex">
 							<b>Mustafa Kasar</b>
-							<p>Lorem, ipsum, dolor sit amet consectetur adipisicing elit. Doloribus, voluptatibus quo facilis unde temporibus.</p>
+							<p>Kasar is Engels docent. Bij Kasar moet je tijdens de les niet op je telefoon zitten want dan word je gelijk de les uitgezet. Zijn lessen bestaan alleen over opdrachten maken en toetsen maken.</p>
 						</div>
 					</div>
 					<div class="teacher">
-						<img src="img/elton.png">
+						<img src="img/elton.png" alt="">
 						<div class="teacherFlex">
 							<b>Elton Boekhoudt</b>
-							<p>Lorem, ipsum, dolor sit amet consectetur adipisicing elit. Doloribus, voluptatibus quo facilis unde temporibus.</p>
-						</div>
-					</div>
-					<div class="teacher">
-						<img src="img/michiel.png">
-						<div class="teacherFlex">
-							<b>Michiel Boerman</b>
-							<p>Lorem, ipsum, dolor sit amet consectetur adipisicing elit. Doloribus, voluptatibus quo facilis unde temporibus.</p>
+							<p>Elton is web docent die naar mijn mening heel goed les kan geven, maar je moet wel opletten tijdens zijn les anders wordt het een zwaar jaar. Ik vind zijn lesmethode heel erg fijn en je wordt echt bij de les betrokken.</p>
 						</div>
 					</div>
 				</div>
@@ -202,7 +175,7 @@
 	<footer>
 		<div class="sectionFooter">
 			<div class="wrapper">
-				<img src="img/curio-16-midden-groen-logo-rgb.png">
+				<img src="img/curio-16-midden-groen-logo-rgb.png" alt="">
 			</div>
 		</div>
 	</footer>
